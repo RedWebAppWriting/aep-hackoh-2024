a18383bc677f5b8777875e010db83369c8672ada
RedWebAppWriting <reddy.336@osu.edu>
2024-10-20 09:45:17 -0400

    Update vadersentiment.py

--- aep-challange/aep_challange/models/vadersentiment.py
+++ aep-challange/aep_challange/models/vadersentiment.py
@@ -18,9 +18,13 @@ nltk.download('punkt_tab')
 
 si = SentimentIntensityAnalyzer()
 
-fields = ['PNT_ATRISKNOTES_TX']
-dataset1=np.array(pd.read_csv("CORE_HackOhio_subset_cleaned_downsampled 1.csv", skipinitialspace=True, usecols=fields))
-#print(dataset1)
+
+fields = ['QUALIFIER_TXT','PNT_ATRISKNOTES_TX']
+df=(pd.read_csv("CORE_HackOhio_subset_cleaned_downsampled 1.csv", skipinitialspace=True, usecols=fields))
+df['combined']= df.apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
+dataset1 = np.array(df['combined'])
+print(dataset1)
+
 
 sentiments = list()
 
@@ -28,34 +32,82 @@ for i in range(len(dataset1)):
     cleaned_text = ''.join(char for char in str(dataset1[i]) if ord(char) < 128)
     sentiments.append(si.polarity_scores(cleaned_text)['compound'])
 
+np.save('vadersentiments.npy',np.array(sentiments))
+
+'''
 with open('vadersentiments.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     for score in sentiments:
         writer.writerow([score])
+'''
 
 print(len(sentiments))
 
-#wordset=set()
+wordset=set()
 #change to range(len(dataset1)):
-for i in range(10):
-    words=word_tokenize(dataset1.iloc[i,0])
-    if '?' in words:
-        words.remove('?')
-    elif '!' in words:
-        words.remove('!')
-    elif '...' in words:
-        words.remove(('...'))
-    elif ',' in words:
-        words.remove(('.'))
-    elif '.' in words:
-        words.remove(('.'))
-    for word in words:
-        wordset.add(word)
+
+sample =20000
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('\n', '', text)
+    stop_words = [
+        'the', 'a', 'an', 'and', 'or', 'but', 'is', 'are', 'was', 'were',
+        'he', 'she', 'it', 'they', 'them', 'that', 'this', 'these', 'those',
+        'there', 'where', 'who', 'whom', 'what', 'which', 'when', 'why',
+        'for', 'to', 'of', 'in', 'on', 'at', 'by', 'with', 'as', 'if',
+        'than', 'about', 'up', 'down', 'out', 'over', 'under', 'between',
+        'also', 'just', 'only', 'so', 'then', 'now', 'like', 'much', 'very',
+        'some', 'any', 'no', 'not', 'too', 'more', 'most'
+    ]
+    # Create regex pattern for stop words
+    stop_words_pattern = r'\b(?:' + '|'.join(stop_words) + r')\b'
+    text = re.sub(stop_words_pattern, '', text)
+    text = re.sub(r'\[name\]', '', text)
+    text = "".join(text)
+    return text
+
+cleaned_texts = []
 
 
-danger=["arc flash", "fire", ""]
+def cleanertext(downsampled)
+    fields = ['QUALIFIER_TXT','PNT_ATRISKNOTES_TX']
+    df=(pd.read_csv("downsampled", skipinitialspace=True, usecols=fields))
+    df['combined']= df.apply(lambda row: ' '.join(row.values.astype(str)), axis=1)
+    dataset1 = np.array(df['combined'])
+    print(dataset1)
+    sample = 20000
+    
+    for i in range(sample):
+        text = clean(dataset1[i])
+        cleaned_texts.append(text)
+    
+        words=word_tokenize(text)
+        for word in words:
+            wordset.add(word)
+    
+    
+    df = pd.DataFrame(cleaned_texts, columns=['cleaned_text'])
+    
+    df.to_csv('cleanedtext.csv',index=False)
 
+    return null;
 
-wordlist = list(wordset)
-print(wordlist)
+wordlist=list(wordset)
 
+countmatrix = np.zeros((sample,len(wordlist)))
+
+for i in range(sample):
+    words=word_tokenize(dataset1[i])
+    for j in range(len(wordlist)):
+        occurence=words.count(wordlist[j])
+        countmatrix[i,j]=occurence
+
+np.save('countmatrix.npy',countmatrix)
+
+df = pd.DataFrame(wordlist,columns=['Words'])
+df.to_csv('wordlist.csv', index=False)
+
+manuallex=["fire", "arc flash", "explosion", "pinched", ]
